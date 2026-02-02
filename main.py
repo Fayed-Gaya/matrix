06d0bf6a9e60c2fab91de78b86a2ac2c624d59d9
Fayed Gaya <gayafayed@gmail.com>
2026-02-02 02:23:24 -0500

    Create level 1 until collisions

---
+++ main.py
@@ -0,0 +1,43 @@
+import pygame
+from sys import exit
+
+pygame.init()
+
+width = 800
+height = 400
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Matrix')
+clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+text_surface = test_font.render('Matrix', False, 'Black').convert()
+
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surface.get_rect(bottomright = (600, 300))
+
+player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80,300))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 50))
+
+    snail_rect.left -= 4
+    if snail_rect.left <= -100: snail_rect.left = 800
+    screen.blit(snail_surface, snail_rect)
+
+    screen.blit(player_surf, player_rect)
+
+    pygame.display.update()
+    clock.tick(60)
+
+
