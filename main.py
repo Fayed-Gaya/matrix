d9b3b59f0d108a5f0915e650211fb01700efe587
Fayed Gaya <gayafayed@gmail.com>
2026-02-04 05:20:48 -0500

    Draw with rectangles (+colors)

--- main.py
+++ main.py
@@ -14,8 +14,9 @@ test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-text_surface = test_font.render('Matrix', False, 'Black').convert()
 
+score_surf = test_font.render('Matrix', False, (64, 64, 64)).convert()
+score_rect = score_surf.get_rect(center = (400, 50))
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(bottomright = (600, 300))
@@ -33,11 +34,11 @@ while True:
         # if event.type == pygame.MOUSEMOTION:
         #     if player_rect.collidepoint(event.pos): print('collision')
 
-
-
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (300, 50))
+    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)
+    screen.blit(score_surf, score_rect)
 
     snail_rect.left -= 4
     if snail_rect.left <= -100: snail_rect.left = 800
