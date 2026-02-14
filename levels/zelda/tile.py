54b30b7a36536065b3b2afb824947127b41ad995
Fayed Gaya <gayafayed@gmail.com>
2026-02-14 11:30:10 -0500

    Setup levels, import graphic assets

---
+++ levels/zelda/tile.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import pygame
+from settings import *
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        image_path = Path(__file__).resolve().parent / "graphics" / "test" / "rock.png"
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.rect = self.image.get_rect(topleft = pos)
