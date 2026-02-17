f0efedfe0f84559ca22c5c14baa05f04d1420007
Fayed Gaya <gayafayed@gmail.com>
2026-02-17 10:55:28 -0500

    Add hitbox, depth based drawing

--- levels/zelda/tile.py
+++ levels/zelda/tile.py
@@ -8,3 +8,4 @@ class Tile(pygame.sprite.Sprite):
         image_path = Path(__file__).resolve().parent / "graphics" / "test" / "rock.png"
         self.image = pygame.image.load(image_path).convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
+        self.hitbox = self.rect.inflate(0, -10)
